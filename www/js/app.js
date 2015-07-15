e472038207eaa8ed7895ed29e418235f5933facd
zafercan <zafercandeli@gmail.com>
2015-07-15 16:32:37 +0300

    pull to refresh

--- www/js/app.js
+++ www/js/app.js
@@ -10,6 +10,7 @@ myApp.run(function($ionicPlatform) {
         }
     });
 });
+
 myApp.config(function($stateProvider, $urlRouterProvider) {
     //set page transition properties
     $stateProvider.state('index', {
@@ -27,28 +28,34 @@ myApp.config(function($stateProvider, $urlRouterProvider) {
     $urlRouterProvider.otherwise("/");
 
 });
+//change srcImage with timestamp
+function refresh(id) {
+    for (var i = 0; i < grids.length; i++) {
+
+        var img = grids[i].srcImage;
+        grids[i].srcImage = '';
+        grids[i].srcImage = grids[i].url + '?timestamp=' + getTimeStamp();
+
+    }
+}
 
 //first page controller
 myApp.controller('ListCtrl', function($scope, $state, $ionicLoading) {
 
     //$ionicHistory.clearCache().then(function(){ $state.go('view', { imageid : 1 } )});
-    
+
+    //refresh button click
     $scope.onRefreshClick = function() {
-        for (var i = 0; i < grids.length; i++) {
-
-            var img = grids[i].srcImage;
-            grids[i].srcImage = '';
-            grids[i].srcImage = grids[i].url + '?timestamp=' + getTimeStamp();
-
-        }
+        refresh();
         $scope.changePage();
     };
+    //take picture button click
     $scope.onTakePictureClick = function() {
-
+        //show spinner
         $ionicLoading.show({
-             content: 'Loading Data',
-        animation: 'fade-in',
-        showBackdrop: false
+            content : 'Loading Data',
+            animation : 'fade-in',
+            showBackdrop : false
         });
         $.getJSON(TAKE_PICTURE_URL, function(data) {
             //console.debug(grids);
@@ -59,13 +66,14 @@ myApp.controller('ListCtrl', function($scope, $state, $ionicLoading) {
                 $scope.hide();
             }
         });
-
+        //hide spinner
         $scope.hide = function() {
             $ionicLoading.hide();
         };
 
     };
-    
+
+    // move main page to second page
     $scope.changePage = function() {
         $state.go('view', {
             imageid : 1
@@ -76,7 +84,7 @@ myApp.controller('ListCtrl', function($scope, $state, $ionicLoading) {
 });
 
 //second page and popup  coontroller
-myApp.controller('MainCtrl', function($scope, $ionicModal, $http, $state, $ionicScrollDelegate) {
+myApp.controller('MainCtrl', function($scope, $ionicModal, $http, $state) {
 
     $scope.hide = [{
         bars : true
@@ -104,51 +112,13 @@ myApp.controller('MainCtrl', function($scope, $ionicModal, $http, $state, $ionic
         $scope.hide.bars = false;
     };
     //ion-refresh function
-    $scope.doRefresh = function() {
+    $scope.pullToRefresh = function() {
 
-        for (var i = 0; i < $scope.grids.length; i++) {
-            var img = $scope.grids[i].srcImage;
-            $scope.grids[i].srcImage = 'http://placehold.it/150x150';
-            console.log("before : " + $scope.grids[i].srcImage);
-            $scope.grids[i].srcImage = img;
-            console.log("after : " + $scope.grids[i].srcImage);
-        }
-        // $state.go('view', {
-        // imageid : 1
-        // },{reload: true});
-        // Stop the ion-refresher from spinning
+        refresh();
+        //stop scrolling
         $scope.$broadcast('scroll.refreshComplete');
 
-        // $http.get("https://dl.dropboxusercontent.com/u/1403240/staj/metadata.json")
-        //
-        // //angular.element(document.getElementById('main')).scope().initialize(grids);
-        //
-        // .success(function(data) {
-        //
-        // var URLs = [];
-        // //var grids;
-        //
-        // var deviceName = data.deviceName;
-        // var raspiCamCount = data.raspiCamCount;
-        //
-        // //$("#imageTitle").innerHTML = "";
-        //
-        // for (var i = 0; i < raspiCamCount; i++) {
-        //
-        // URLs[i] = data.cams[i].url;
-        // $scope.grids[i].srcImage = URLs[i];
-        // $scope.grids[0].title = deviceName;
-        // $scope.grids[i].srcImage = 'http://placehold.it/150x150';
-        //
-        // //document.getElementById("imageTitle").innerHTML = deviceName;
-        // //$("#images").append("<li class=\"item\" ><img src=' " + URLs[i] + " ' width='100%' heigth='100%'></li> ");
-        // }
-        // }).finally(function() {
-        //
-        // // Stop the ion-refresher from spinning
-        // $scope.$broadcast('scroll.refreshComplete');
-        // });
     };
-    $ionicScrollDelegate.resize();
+
 });
 
--- www/js/json.js
+++ www/js/json.js
@@ -10,23 +10,23 @@ var TAKE_PICTURE_URL = BASE_URL + "/refresh";
 //default grids
 grids = [{
     id : 0,
-    url:'http://placehold.it/150x150',
+    url : 'http://placehold.it/150x150',
     srcImage : 'http://placehold.it/150x150',
     srcShelf : 'img/Shelf4_1.png',
     title : 'Images',
 }, {
     id : 1,
-    url:'http://placehold.it/150x150',
+    url : 'http://placehold.it/150x150',
     srcImage : 'http://placehold.it/150x150',
     srcShelf : 'img/Shelf4_2.png',
 }, {
     id : 2,
-    url:'http://placehold.it/150x150',
+    url : 'http://placehold.it/150x150',
     srcImage : 'http://placehold.it/150x150',
     srcShelf : 'img/Shelf4_3.png',
 }, {
     id : 3,
-    url:'http://placehold.it/150x150',
+    url : 'http://placehold.it/150x150',
     srcImage : 'http://placehold.it/150x150',
     srcShelf : 'img/Shelf4_4.png',
 }];
@@ -43,16 +43,16 @@ function JSONProcess() {
         //get camera count
         var raspiCamCount = data.raspiCamCount;
         var usbCamCount = data.usbCamCount;
-        
+
         for (var i = 0; i < usbCamCount; i++) {
             //get urls
             URLs[i] = data.cams[i].url;
-/*
+            /*
             URLs[0] = "http://1.bp.blogspot.com/-pPAGNyln86Y/Uindkpv87kI/AAAAAAAAI4I/pbUilrlqoOg/s1600/Tulips2.jpg";
             URLs[1] = "http://www.steves-digicams.com/sdc-classic/qv2300/samples/07220016.jpg";
             URLs[2] = "http://drop.ndtv.com/albums/BUSINESS/geneva2013/laferrari-700.jpg";
             URLs[3] = "http://www.personal-view.com/talks/uploads/FileUpload/39/5efeafb5213ce3433d0b2fc7fe72cb.jpg";
-*/
+            */
             //update grids
             grids[i].srcImage = URLs[i];
             grids[i].url = URLs[i];
@@ -61,11 +61,11 @@ function JSONProcess() {
             var fileName = "image";
             var index = i.toString();
             fileName = fileName + index;
- 
+
             //download  file
-            
+
             //download( URLs[i], "GrundigImages", fileName);
-       /*
+            /*
              var image = "file:///storage/emulated/0//GrundigImages/image";
              var index = grids[i].id.toString();
              image += index;
@@ -76,17 +76,9 @@ function JSONProcess() {
     });
 }
 
+//return timestamp in ms
 function getTimeStamp() {
     var d = new Date();
     var n = d.getTime();
     return n;
-}
-
-function clearContent(id) {
-    alert("Clear");
-    //  grids[0].srcImage = 'http://placehold.it/150x150';
-
-    document.getElementById(id).innerHTML = "";
-    alert("done");
-
 }
